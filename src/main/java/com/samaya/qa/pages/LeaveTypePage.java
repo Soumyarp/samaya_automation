469c322616411c3ac0220ffa61d44bcaee1c6a26
Soumya Ranjan <ranjan@securax.in>
2017-11-23 15:52:40 +0530

    created leavetype functionality and added in suite

--- src/main/java/com/samaya/qa/pages/LeaveTypePage.java
+++ src/main/java/com/samaya/qa/pages/LeaveTypePage.java
@@ -1,5 +1,70 @@
 package com.samaya.qa.pages;
 
-public class LeaveTypePage {
+import java.util.List;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.FindBys;
+import org.openqa.selenium.support.PageFactory;
+
+import com.samaya.qa.base.TestBase;
+
+public class LeaveTypePage extends TestBase {
+	
+	@FindBy(xpath="//span[contains(text(),'Leave Types ')]")
+	WebElement leaveTypeLebel;
+	
+	@FindBy(xpath="//st-button[@type='button']")
+	WebElement newLeaveTypeButton;
+	
+	@FindBy(xpath="//div[@class='dropdown']/div/button")
+	WebElement clickonDropdown;
+	
+	@FindBys({
+		@FindBy(xpath="//div[1]/st-dropdownlist/div/ul/li")})
+		List<WebElement> selectContentfrmdrpdwn;
+	
+	@FindBy(xpath="//input[@placeholder='Name']")
+	WebElement leaveTypeName;
+	
+	@FindBy(xpath="//input[@placeholder='Abbreviation']")
+	WebElement leaveTypeabbrevation;
+	
+	@FindBy(xpath ="//button[contains(.,'SAVE')]")
+    WebElement saveButon;
+	
+	@FindBy(xpath="//*[@id='appContentContainer']/div/app-leave-type-search/app-leave-type-details/div/div[1]/button")
+	WebElement closeLeaveTypeScreen;
+	
+	
+	//Intializing the page objects	
+	public LeaveTypePage(){
+		PageFactory.initElements(driver, this);
+	}
+	
+	public void clickLeaveType(){
+		newLeaveTypeButton.click();
+	}
+	
+	public void clickonSelectAffiliate(int arg)
+	{
+		selectContentfrmdrpdwn.get(arg).click();
+	}
+	
+	public void closeLeaveTypeScreen(){
+		closeLeaveTypeScreen.click();
+	}
+	
+	public void createNewLeaveType(String name, String abbrevation) throws InterruptedException{
+		clickonDropdown.click();
+		Thread.sleep(2000);
+		clickonSelectAffiliate(1);
+		leaveTypeName.sendKeys(name);
+		leaveTypeabbrevation.sendKeys(abbrevation);
+		Thread.sleep(2000);
+		saveButon.click();
+		Thread.sleep(7000);
+		
+	}
 
 }
--- src/test/java/com/samaya/qa/testcases/LeaveTypePageTest.java
+++ src/test/java/com/samaya/qa/testcases/LeaveTypePageTest.java
@@ -1,5 +1,64 @@
 package com.samaya.qa.testcases;
 
-public class LeaveTypePageTest {
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import com.samaya.qa.base.TestBase;
+import com.samaya.qa.pages.HomePage;
+import com.samaya.qa.pages.LeaveTypePage;
+import com.samaya.qa.pages.LoginPage;
+import com.samaya.qa.pages.ManagePage;
+import com.samaya.qa.util.TestUtil;
+
+public class LeaveTypePageTest extends TestBase {
+	HomePage homepage;
+	LoginPage loginpage;
+	TestUtil utility;
+	LeaveTypePage leavetypepage;
+	
+	String sheetname = "LeaveType";
+	
+	@Test(priority=13)
+	public void verifyManagePage() throws InterruptedException{
+		loginpage = new LoginPage();
+		loginpage.login(prop.getProperty("username"), prop.getProperty("password"));
+		homepage=new HomePage();
+		Thread.sleep(2000);
+		homepage.clickOnOrganisation();
+		homepage.clickOnLeaveTypePage();
+		leavetypepage= new LeaveTypePage();
+		Thread.sleep(1000);
+		leavetypepage.clickLeaveType();
+	}      
+	@DataProvider
+	public Object[][] getData(){
+		Object data[][]=utility.getTestData(sheetname);
+		return data;
+}
+	@Test(priority=14,dataProvider="getData")
+	public  void validateLeaveType(String LeaveName,String LeaveAbbrevation ) throws InterruptedException{
+		leavetypepage.createNewLeaveType(LeaveName, LeaveAbbrevation);;
+	}
+	
+	@Test(priority=15)
+	public void logoutManageScreen() throws InterruptedException{
+		leavetypepage.closeLeaveTypeScreen();
+		Thread.sleep(2000);
+	//	homepage.clickOnOrganisation();
+	//	Thread.sleep(2000);
+	//	homepage.clickOnAffiliatePage();
+	//	Thread.sleep(2000);
+		
+		
+	}
+	@AfterClass
+	public void logout() throws InterruptedException
+	{
+		homepage.clickUsername();
+		Thread.sleep(3000);
+		homepage.clickLogoutButton(0);
+		Thread.sleep(10000);	
+	}
 
 }
